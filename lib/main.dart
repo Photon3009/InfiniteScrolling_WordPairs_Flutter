e384cee3e7c3caed21137afecaddc8b875376d0e
Photon3009 <sv30092001@gmail.com>
2022-08-31 16:18:24 +0530

    Added functionionality to add specific words to favourite

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.lime),
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: MyApp1(),
     );
   }
@@ -39,6 +39,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved=<WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -52,11 +53,26 @@ class _RandomWordsState extends State<RandomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10)); 
         }
+        final alreadySaved = _saved.contains(_suggestions[index]);
         return ListTile(
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
           ),
+            trailing: Icon(    // NEW from here ...
+    alreadySaved ? Icons.favorite : Icons.favorite_border,
+    color: alreadySaved ? Colors.red : null,
+    semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+  ),
+  onTap: () {          // NEW from here ...
+    setState(() {
+      if (alreadySaved) {
+        _saved.remove(_suggestions[index]);
+      } else {
+        _saved.add(_suggestions[index]);
+      }
+    });                // to here.
+  },
         
         );
       },
